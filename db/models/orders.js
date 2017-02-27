32aabbc25d42edba658fa7d4557a35ed419b1c6e
Soren Yeadon <spyeadon@gmail.com>
2017-02-27 17:28:44 -0600

    finished orders model draft 1

--- db/models/orders.js
+++ db/models/orders.js
@@ -1,1 +1,26 @@
 const db = require('../index.js');
+const Sequelize = require('sequelize');
+
+const Orders = db.define('orders', {
+  status: {
+    type: Sequelize.STRING,
+    defaultValue: 'created',
+    validate: {
+      allowNull: false
+    }
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    defaultValue: 0.00
+  }
+}, {
+  instanceMethods: {
+    changeStatus: function (cancelled) {
+      if (this.status === 'created') this.status = 'processing'
+      else if (cancelled === true && this.status === 'processing') this.status = 'pancelled'
+    }
+  }
+});
+
+module.exports = Orders;
