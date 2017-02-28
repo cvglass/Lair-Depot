5ef4563afab8ee1e49da0a61c9b916b727a22b65
Soren Yeadon <spyeadon@gmail.com>
2017-02-28 11:03:16 -0600

    finished orders model tests, it worked!

---
+++ db/model_tests/orders.test.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const db = require('APP/db');
+const Orders = require('../models/orders.js');
+const {expect} = require('chai');
+
+describe('Orders`', () => {
+  before('wait for the db', () => db.didSync)
+
+  describe('orders model definition test ', () => {
+
+    var newOrder;
+    var newestOrder;
+
+    beforeEach(function () {
+      newOrder = Orders.build({})
+      newestOrder = Orders.build({})
+    })
+
+    it('schema design test', () => {
+      expect(newOrder.price).to.equal(0.00);
+      expect(newOrder.status).to.equal('created');
+      expect(newestOrder.price).to.not.equal(1);
+      expect(newestOrder.price).to.not.equal('not created');
+    });
+
+    it('tests change status instance method', () => {
+      newestOrder.changeStatus();
+      newOrder.changeStatus();
+      expect(newOrder.status).to.equal('processing');
+      newOrder.changeStatus('cancel');
+      expect(newOrder.status).to.equal('cancelled');
+      newestOrder.changeStatus('complete');
+      expect(newestOrder.status).to.equal('completed');
+    });
+  });
+});
