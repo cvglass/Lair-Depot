c471d16b3224ba608f1f000c12d9c18b67b188c5
Christine <christinevglass@gmail.com>
2017-03-01 17:08:48 -0600

    Update address.js
    changed notEmpty to allowNull.

--- db/models/address.js
+++ db/models/address.js
@@ -5,38 +5,38 @@ const db = require('APP/db');
 
 const Address = db.define('addresses',
   {
-    //Use allowNull since it applies to the DBMS
     street: {
       type: Sequelize.STRING,
       validate: {
-        notEmpty: true,
+        allowNull: false,
       }
     },
     city: {
       type: Sequelize.STRING,
       validate: {
-        notEmpty: true,
+        allowNull: false,
       }
     },
-    zip: {// int or string? check length of this?
+    zip: {// going to keep as a string because Sequelize can't validate integers
       type: Sequelize.STRING,
       validate: {
-        notEmpty: true,
-        len: [5,5], //what about 9 digit zips?
-        not: ["[a-z]",'i'],
+        allowNull: false,
+        len: [5,9],
+        isInt: true,
       }
     },
     state: {
       type: Sequelize.STRING,
       validate: {
-        notEmpty: true, //max length 2
+        allowNull: false,
+        len: [2,2]
       }
     },
     phone: {
       type: Sequelize.STRING,
       validate: {
-        notEmpty: true,
-        not: ["[a-z]",'i'], //can you say only numbers instead? what about symbols? maybe isNumeric: true instead.
+        allowNull: false,
+        isInt: true,
       }
     }
   },
