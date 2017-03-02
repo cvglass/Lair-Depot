2a8f9b5824bc2d372473152712c39aa60b499a60
ray coti <Raymond.coti@gmail.com>
2017-03-02 11:09:56 -0600

    everything seems fine

--- db/models/address.js
+++ db/models/address.js
@@ -7,34 +7,34 @@ const Address = db.define('addresses',
   {
     street: {
       type: Sequelize.STRING,
+      allowNull: false,
       validate: {
-        allowNull: false,
       }
     },
     city: {
       type: Sequelize.STRING,
+      allowNull: false,
       validate: {
-        allowNull: false,
       }
     },
     zip: {// going to keep as a string because Sequelize can't validate integers
       type: Sequelize.STRING,
+      allowNull: false,
       validate: {
-        allowNull: false,
         len: [5,9],
         isInt: true,
       }
     },
     state: {
       type: Sequelize.STRING,
+      allowNull: false,
       validate: {
-        allowNull: false,
       }
     },
     phone: {
       type: Sequelize.STRING,
+      allowNull: false,
       validate: {
-        allowNull: false,
         isInt: true,
       }
     }
