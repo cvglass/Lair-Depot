a98346ff4bddb66abd5fa75ee57d2a92d2d89a73
cvglass <christinevglass@gmail.com>
2017-03-04 10:37:18 -0600

    placeholder user component

---
+++ app/components/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = ({ user }) => {
+  return (
+    <div>
+      <p>placeholder. need to figure out how to access userId</p>
+    </div>
+  )
+}
+
+export default User
